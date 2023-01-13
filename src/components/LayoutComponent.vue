c957a63d33b322a392d17cb9d4bfff32203425ce
m911 <mitaka911@gmail.com>
2023-01-13 02:12:04 +0200

    1

--- src/components/LayoutComponent.vue
+++ src/components/LayoutComponent.vue
@@ -5,8 +5,12 @@ import { RouterLink, RouterView } from "vue-router";
 <template>
   <header>
     <nav>
-      <RouterLink class="link" to="/">Home</RouterLink>
-      <RouterLink to="/login">Login</RouterLink>
+      <a class="router-link-exact-active">
+        <RouterLink class="link" to="/">Home</RouterLink>
+      </a>
+      <a class="router-link-exact-active">
+        <RouterLink to="/login">Login</RouterLink>
+      </a>
     </nav>
     <img
       alt="Vue logo"
@@ -18,4 +22,59 @@ import { RouterLink, RouterView } from "vue-router";
   </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+  margin: 0 2rem 0 0;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+  text-align: left;
+  margin-left: -1rem;
+  font-size: 1rem;
+  padding: 1rem 0;
+  margin-top: 1rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
